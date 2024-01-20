b393920cdbca5db6201a5a5497dba4a028492922
Zhila Shariat <zshariat08@gmail.com>
2024-01-19 23:44:07 -0800

    updated hello for name

--- hello_test.go
+++ hello_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 // For now, it's enough to know that your t of type *testing.T is your "hook" into the testing framework so you can do things like t.Fail() when you want to fail.
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
